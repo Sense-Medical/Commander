b456f2995733dd909cbadca8e291a71b1ca6fa9e
Robert Payne <robertpayne@me.com>
2017-09-13 17:19:10 +1200

    Package Updates

--- Package.swift
+++ Package.swift
@@ -1,8 +1,19 @@
+// swift-tools-version:4.0
+
+import Foundation
 import PackageDescription
 
 let package = Package(
   name: "Commander",
+  products: [
+    .library(name: "Commander", targets: ["Commander"])
+  ],
   dependencies: [
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
+    // https://github.com/apple/swift-package-manager/pull/597
+    .package(url: "https://github.com/Sense-Medical/Spectre.git", .branch("cortex")),
+  ],
+  targets: [
+    .target(name: "Commander", path: "Sources"),
+    .testTarget(name: "CommanderTests", dependencies: ["Commander", "Spectre"], path: "Tests/CommanderTests")
   ]
 )
